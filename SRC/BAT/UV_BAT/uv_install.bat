7d1c125c65769a497fc5ee7d78d0529851c325bf
lisitsinyr <lisitsinyr@gmail.com>
2025-12-21 18:25:11 +0400

    20251221182510

---
+++ SRC/BAT/UV_BAT/uv_install.bat
@@ -0,0 +1,4 @@
+rem powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+rem C:\Program Files\PowerShell\7\pwsh.exe
+rem C:\Users\lyr\AppData\Local\Microsoft\WindowsApps\pwsh.exe
+pwsh -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
